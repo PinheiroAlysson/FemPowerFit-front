1b09c4ca9381e8a295b5893f06db51b6714ddf23
Gabriel <eder.silva@montreal.com.br>
2024-04-16 16:41:30 -0300

    resolvendo os BO da anna

--- fempowerfitweb/src/app/layout.tsx
+++ fempowerfitweb/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
+      <body className={`${inter.className} text-white`}>
         <main>
           {children}
         </main>
